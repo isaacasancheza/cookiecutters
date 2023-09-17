3072ea721fca7f10ddaca9645abe8bd3a0c7d507
Isaac Sánchez <isaacsancheza@outlook.com>
2023-09-17 18:23:44 +0000

    Update cookiecutters to use pydantic-settings

--- aws-cdk/{{ cookiecutter.stack_name }}/app.py
+++ aws-cdk/{{ cookiecutter.stack_name }}/app.py
@@ -1,13 +1,20 @@
-from os import getenv
-
 import aws_cdk as cdk
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from stacks.root import RootStack
 
 
-app = cdk.App()
-env = cdk.Environment(account=getenv('ACCOUNT'), region=getenv('REGION'))
+class Settings(BaseSettings):
+    region: str
+    account: str
+    
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
+
+app = cdk.App()
+settings = Settings()
+
+env = cdk.Environment(region=settings.region, account=settings.account)
 RootStack(app, 'RootStack', stack_name='{{ cookiecutter.stack_name }}', env=env)
 
 app.synth()
