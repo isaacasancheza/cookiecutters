a3d8813618d471cb571045cc784ac8e2e22be0dd
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-09-18 18:02:24 -0600

    Update {{ cookiecutter.stack_name }}.py

--- aws-cdk-stack-lambda/{{ cookiecutter.stack_name }}/{{ cookiecutter.stack_name }}.py
+++ aws-cdk-stack-lambda/{{ cookiecutter.stack_name }}/{{ cookiecutter.stack_name }}.py
@@ -4,20 +4,21 @@ from constructs import Construct
 
 
 class LambdaFunction(Construct):
-    def __init__(self, scope: Construct, construct_id: str, source_code_path: str, timeout: int = 60) -> None:
+    def __init__(self, scope: Construct, construct_id: str, source_code_path: str, timeout: int = 60, **kwargs) -> None:
         super().__init__(scope, construct_id)
 
         self.function: lambda_.Function = lambda_.Function(
             self, 
             construct_id, 
             code=lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
-                image=lambda_.Runtime.PYTHON_3_10.bundling_image, 
+                image=lambda_.Runtime.PYTHON_3_11.bundling_image, 
                 command=['bash', '-c', 'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
             ])),
             handler='lambda.handler',
             timeout=cdk.Duration.seconds(timeout),
             tracing=lambda_.Tracing.ACTIVE,
             runtime=lambda_.Runtime.PYTHON_3_11,
+            **kwargs,
         )
 
 
