b6461e41a14c403111dcee8b0de3fb45466b7b4d
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-09-18 18:01:16 -0600

    Update {{ cookiecutter.stack_name }}.py

--- aws-cdk-stack-lambda/{{ cookiecutter.stack_name }}/{{ cookiecutter.stack_name }}.py
+++ aws-cdk-stack-lambda/{{ cookiecutter.stack_name }}/{{ cookiecutter.stack_name }}.py
@@ -7,17 +7,18 @@ class LambdaFunction(Construct):
     def __init__(self, scope: Construct, construct_id: str, source_code_path: str, timeout: int = 60) -> None:
         super().__init__(scope, construct_id)
 
-        function_kwargs = {
-            'code': lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
+        self.function: lambda_.Function = lambda_.Function(
+            self, 
+            construct_id, 
+            code=lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
                 image=lambda_.Runtime.PYTHON_3_10.bundling_image, 
                 command=['bash', '-c', 'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
             ])),
-            'handler': 'lambda.handler',
-            'timeout': cdk.Duration.seconds(timeout),
-            'tracing': lambda_.Tracing.ACTIVE,
-            'runtime': lambda_.Runtime.PYTHON_3_10,
-        }
-        self.function: lambda_.Function = lambda_.Function(self, construct_id, **function_kwargs)
+            handler='lambda.handler',
+            timeout=cdk.Duration.seconds(timeout),
+            tracing=lambda_.Tracing.ACTIVE,
+            runtime=lambda_.Runtime.PYTHON_3_11,
+        )
 
 
 class {{ cookiecutter.stack_name.split('_') | map('capitalize') | join('') }}Stack(cdk.NestedStack):
