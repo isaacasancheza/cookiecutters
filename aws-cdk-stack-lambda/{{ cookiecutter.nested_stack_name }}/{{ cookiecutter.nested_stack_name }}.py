969fc80c3670c253361cf5bd2bd22d953a735858
Isaac Sánchez <isaacsancheza@outlook.com>
2023-10-20 21:42:17 -0600

    Use new style and add lambda layer; use rsync for packing

---
+++ aws-cdk-stack-lambda/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
@@ -0,0 +1,50 @@
+import aws_cdk as cdk
+from aws_cdk import aws_lambda as lambda_
+from constructs import Construct
+
+
+class LambdaLayer(Construct):
+    def __init__(self, scope: Construct, construct_id: str, /, *, source_code_path: str, **kwargs) -> None:
+        super().__init__(scope, construct_id)
+
+        self.layer: lambda_.LayerVersion = lambda_.LayerVersion(
+            self,
+            construct_id,
+            code=lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
+                image=lambda_.Runtime.PYTHON_3_11.bundling_image, 
+                command=['bash', '-c', 'pip install -r requirements.txt -t /asset-output/python',
+            ])),
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            compatible_runtimes=[
+                lambda_.Runtime.PYTHON_3_11,
+            ],
+            compatible_architectures=[
+                lambda_.Architecture.X86_64,
+            ],
+            **kwargs,
+        )
+
+
+class LambdaFunction(Construct):
+    def __init__(self, scope: Construct, construct_id: str, /, *, source_code_path: str, timeout: int = 60, **kwargs) -> None:
+        super().__init__(scope, construct_id)
+
+        self.function: lambda_.Function = lambda_.Function(
+            self, 
+            construct_id, 
+            code=lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
+                image=lambda_.Runtime.PYTHON_3_11.bundling_image, 
+                command=['bash', '-c', 'rsync -r . /asset-output'],
+            )),
+            handler='lambda.handler',
+            timeout=cdk.Duration.seconds(timeout),
+            tracing=lambda_.Tracing.ACTIVE,
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            memory_size=512,
+            **kwargs,
+        )
+
+
+class {{ cookiecutter.nested_stack_name.split('_') | map('capitalize') | join('') }}NestedStack(cdk.NestedStack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
