6f7163f71a4b19192f70ab3a1e983bc7a9d5884c
Isaac Sánchez <isaacsancheza@outlook.com>
2023-10-25 16:08:45 -0600

    Remove variable type typing

--- aws-chalice/{{ cookiecutter.stack_name }}/nested_stacks/chalice.py
+++ aws-chalice/{{ cookiecutter.stack_name }}/nested_stacks/chalice.py
@@ -7,7 +7,7 @@ class ChaliceNestedStack(cdk.NestedStack):
     def __init__(self, scope: Construct, construct_id: str, /, *, source_dir: str = 'chalice') -> None:
         super().__init__(scope, construct_id)
         
-        self.app: Chalice = Chalice(
+        self.app = Chalice(
             self, 
             'App', 
             source_dir=source_dir,
