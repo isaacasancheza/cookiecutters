a992ee6c89d017088c688b94877a23a8c089fbaf
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-11-04 11:36:02 -0600

    Update {{ cookiecutter.nested_stack_name }}.py

--- aws-cdk-nested-stack-lambda/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
+++ aws-cdk-nested-stack-lambda/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
@@ -69,5 +69,11 @@ class LambdaFunction(Construct):
 
 
 class {{ cookiecutter.nested_stack_name.split('_') | map('capitalize') | join('') }}NestedStack(cdk.NestedStack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+            self, 
+            scope: Construct, 
+            construct_id: str,
+            /,
+            **kwargs,
+        ) -> None:
         super().__init__(scope, construct_id, **kwargs)
