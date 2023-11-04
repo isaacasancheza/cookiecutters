95b90885e2d121e26b206c304d15dc46c056cb65
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-11-04 11:28:13 -0600

    Update stack.py

--- aws-cdk-stack/{{ cookiecutter.stack_name }}/stack.py
+++ aws-cdk-stack/{{ cookiecutter.stack_name }}/stack.py
@@ -3,5 +3,10 @@ from constructs import Construct
 
 
 class Stack(cdk.Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+            self, 
+            scope: Construct, 
+            construct_id: str, 
+            **kwargs
+        ) -> None:
         super().__init__(scope, construct_id, **kwargs)
