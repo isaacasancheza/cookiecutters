413f0e0b16ed427ea37090c422baeab4a70543a9
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-11-04 11:30:20 -0600

    Update {{ cookiecutter.nested_stack_name }}.py

--- aws-cdk-nested-stack/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
+++ aws-cdk-nested-stack/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
@@ -3,5 +3,10 @@ from constructs import Construct
 
 
 class {{ cookiecutter.nested_stack_name.split('_') | map('capitalize') | join('') }}NestedStack(cdk.NestedStack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+            self, 
+            scope: Construct, 
+            construct_id: str, 
+            **kwargs,
+        ) -> None:
         super().__init__(scope, construct_id, **kwargs)
