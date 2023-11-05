a054ad19a5ca1fa41fee6a982653f7aa281d6942
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-11-05 12:14:40 -0600

    Update {{ cookiecutter.nested_stack_name }}.py

--- aws-cdk-nested-stack-lambda/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
+++ aws-cdk-nested-stack-lambda/{{ cookiecutter.nested_stack_name }}/{{ cookiecutter.nested_stack_name }}.py
@@ -10,25 +10,18 @@ class LambdaLayer(Construct):
             construct_id: str, 
             /, 
             *, 
-            is_package: bool = False, 
             source_code_path: str, 
             **kwargs,
         ) -> None:
         super().__init__(scope, construct_id)
-
-        if is_package:
-            command = ['bash', '-c', 'pip install . -t /asset-output/python']
-        else:
-            command = ['bash', '-c', 'pip install -r requirements.txt -t /asset-output/python']
 
         self.layer = lambda_.LayerVersion(
             self,
             construct_id,
             code=lambda_.Code.from_asset(source_code_path, bundling=cdk.BundlingOptions(
                 image=lambda_.Runtime.PYTHON_3_11.bundling_image, 
-                command=command,
+                command=['bash', '-c', 'pip install -r requirements.txt -t /asset-output/python'],
             )),
-            removal_policy=cdk.RemovalPolicy.DESTROY,
             compatible_runtimes=[
                 lambda_.Runtime.PYTHON_3_11,
             ],
@@ -47,7 +40,6 @@ class LambdaFunction(Construct):
             /, 
             *, 
             source_code_path: str, 
-            timeout: int = 60, 
             **kwargs,
         ) -> None:
         super().__init__(scope, construct_id)
@@ -60,10 +52,7 @@ class LambdaFunction(Construct):
                 command=['bash', '-c', 'rsync -r . /asset-output'],
             )),
             handler='lambda.handler',
-            timeout=cdk.Duration.seconds(timeout),
-            tracing=lambda_.Tracing.ACTIVE,
             runtime=lambda_.Runtime.PYTHON_3_11,
-            memory_size=512,
             **kwargs,
         )
 
